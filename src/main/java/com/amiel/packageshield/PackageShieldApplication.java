4cbacbb0e9ac0a6fb873980a0a8232a94a594ee3
AmielRe <amieli1999@gmail.com>
2023-06-11 17:55:58 +0300

    Create basic spring application as project skeleton.

---
+++ src/main/java/com/amiel/packageshield/PackageShieldApplication.java
@@ -0,0 +1,13 @@
+package com.amiel.packageshield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageShieldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PackageShieldApplication.class, args);
+    }
+
+}
