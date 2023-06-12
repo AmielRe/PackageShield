227c5c7adc34b18c55be996976f1c0a7a85f563d
AmielRe <amieli1999@gmail.com>
2023-06-12 19:34:16 +0300

    Add logs and javadoc.

--- src/main/java/com/amiel/packageshield/controllers/ScanController.java
+++ src/main/java/com/amiel/packageshield/controllers/ScanController.java
@@ -1,10 +1,12 @@
 package com.amiel.packageshield.controllers;
 
-import com.amiel.packageshield.handlers.Handler;
+import com.amiel.packageshield.handlers.IHandler;
 import com.amiel.packageshield.payload.request.ScanRequest;
 import com.amiel.packageshield.payload.response.IResponse;
 import com.amiel.packageshield.payload.response.MessageResponse;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -14,9 +16,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/api/v1")
 public class ScanController implements ApplicationContextAware {
+
+    private static final Logger logger = LoggerFactory.getLogger(ScanController.class);
 
     private ApplicationContext applicationContext;
 
@@ -37,13 +42,18 @@ public class ScanController implements ApplicationContextAware {
     @PostMapping("/scan")
     public ResponseEntity<IResponse> scan(@Valid @RequestBody final ScanRequest scanRequest) {
         try {
+            logger.info(String.format("Received new scan request for '%s' ecosystem.", scanRequest.getEcosystem()));
             // Get appropriate handler according to ecosystem (handle case-sensitive)
-            Handler handler = getContext().getBean(scanRequest.getEcosystem().toLowerCase(), Handler.class);
-            return handler.handle(scanRequest);
-        } catch(BeansException ignored) {
-            return ResponseEntity.badRequest().body(new MessageResponse(String.format("Invalid param value - '%s' ecosystem is not supported currently", scanRequest.getEcosystem())));
+            IHandler IHandler = getContext().getBean(scanRequest.getEcosystem().toLowerCase(), IHandler.class);
+            return IHandler.handle(scanRequest);
+        } catch(BeansException ex) {
+            String errMsg = String.format("Invalid param value - '%s' ecosystem is not supported currently", scanRequest.getEcosystem());
+            logger.error(errMsg, ex);
+            return ResponseEntity.badRequest().body(new MessageResponse(errMsg));
         } catch(Exception ex) {
-            return ResponseEntity.badRequest().body(new MessageResponse(ex.getMessage()));
+            String errMsg = String.format("Error occurred while trying to process scan request for %s ecosystem", scanRequest.getEcosystem());
+            logger.error(errMsg, ex);
+            return ResponseEntity.badRequest().body(new MessageResponse(errMsg));
         }
     }
 }
--- src/main/java/com/amiel/packageshield/handlers/NpmHandler.java
+++ src/main/java/com/amiel/packageshield/handlers/NpmHandler.java
@@ -6,21 +6,36 @@ import com.amiel.packageshield.payload.response.ScanResponse;
 import com.amiel.packageshield.models.VulnerableDependency;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import java.io.IOException;
 import java.util.*;
 
 @Component("npm")
 public class NpmHandler extends BaseHandler {
 
+    private final String PACKAGE_JSON_DEPENDENCIES_ROOT = "dependencies";
+
+    private static final Logger logger = LoggerFactory.getLogger(NpmHandler.class);
+
+    /**
+     * Get file content and ecosystem, parse it as package.json file and scan each dependency for
+     * vulnerabilities using all scanners
+     * @param decodedFileContent File content param after base64 decode
+     * @param ecoSystem Ecosystem param (
+     * @return IResponse object containing either the ScanResponse with the VulnerableDependencies or MessageResponse
+     * With error
+     */
     @Override
     protected ResponseEntity<IResponse> customHandleLogic(final String decodedFileContent, final String ecoSystem) {
         ScanResponse response = new ScanResponse(new ArrayList<>());
         List<Dependency> dependenciesToScan = parseFileContent(decodedFileContent);
+        logger.info(String.format("Found %d dependencies to scan, starting scan now", dependenciesToScan.size()));
         for (Dependency dep : dependenciesToScan) {
             List<VulnerableDependency> vulnerableDependencies = super.scanWithAllScanners(dep, ecoSystem);
 
+            // Iterate over all vulnerable dependencies found and only add new ones.
             for (VulnerableDependency currVulDep : vulnerableDependencies) {
                 if(response.getVulnerableDependencies().stream().noneMatch(vulnerableDependency -> vulnerableDependency.getName().equalsIgnoreCase(currVulDep.getName()))){
                     response.addVulnerablePackage(currVulDep);
@@ -28,6 +43,7 @@ public class NpmHandler extends BaseHandler {
             }
         }
 
+        logger.info(String.format("Finally, found %d vulnerable dependencies", response.getVulnerableDependencies().size()));
         return ResponseEntity.ok().body(response);
     }
 
@@ -42,7 +58,7 @@ public class NpmHandler extends BaseHandler {
         try {
             ObjectMapper mapper = new ObjectMapper();
             JsonNode rootNode = mapper.readTree(packageJsonString);
-            JsonNode dependenciesNode = rootNode.path("dependencies");
+            JsonNode dependenciesNode = rootNode.path(PACKAGE_JSON_DEPENDENCIES_ROOT);
 
             Iterator<Map.Entry<String, JsonNode>> dependenciesIterator = dependenciesNode.fields();
             while (dependenciesIterator.hasNext()) {
@@ -52,8 +68,8 @@ public class NpmHandler extends BaseHandler {
 
                 packages.add(new Dependency(packageName, packageVersion));
             }
-        } catch (IOException e) {
-            // Todo: Log later
+        } catch (Exception e) {
+            logger.error("Error occurred while trying to extract dependencies from file content", e);
         }
 
         return packages;
