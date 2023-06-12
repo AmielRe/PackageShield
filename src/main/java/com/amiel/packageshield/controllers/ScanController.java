30fc97e343e7b14b0b4f19bd97f38dcd95dc0550
AmielRe <amieli1999@gmail.com>
2023-06-12 18:03:20 +0300

    Wrap error with MessageResponse.
    Remove code that was moved.
    Wrap error with MessageResponse (so that it will be in serialized to json).

--- src/main/java/com/amiel/packageshield/controllers/ScanController.java
+++ src/main/java/com/amiel/packageshield/controllers/ScanController.java
@@ -2,21 +2,17 @@ package com.amiel.packageshield.controllers;
 
 import com.amiel.packageshield.handlers.Handler;
 import com.amiel.packageshield.payload.request.ScanRequest;
-import jakarta.annotation.PostConstruct;
+import com.amiel.packageshield.payload.response.IResponse;
+import com.amiel.packageshield.payload.response.MessageResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.graphql.client.GraphQlClient;
-import org.springframework.graphql.client.HttpGraphQlClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -24,19 +20,8 @@ public class ScanController implements ApplicationContextAware {
 
     private ApplicationContext applicationContext;
 
-    @Value("${graphqlApiUrl}")
-    private String graphqlApiUrl;
-
-    GraphQlClient client;
-
-    @PostConstruct
-    void init() {
-        WebClient wc = WebClient.create(graphqlApiUrl);
-        client = HttpGraphQlClient.create(wc);
-    }
-
     @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+    public void setApplicationContext(final ApplicationContext applicationContext) throws BeansException {
         this.applicationContext = applicationContext;
     }
 
@@ -44,14 +29,21 @@ public class ScanController implements ApplicationContextAware {
         return applicationContext;
     }
 
+    /**
+     * Scan given dependencies for vulnerabilities.
+     * @param scanRequest Request containing the "ecosystem" and the file content
+     * @return Response - either a message (probably error) or ScanResponse with all vulnerabilities
+     */
     @PostMapping("/scan")
-    public ResponseEntity<String> signIn(@Valid @RequestBody ScanRequest scanRequest) {
+    public ResponseEntity<IResponse> scan(@Valid @RequestBody final ScanRequest scanRequest) {
         try {
             // Get appropriate handler according to ecosystem (handle case-sensitive)
             Handler handler = getContext().getBean(scanRequest.getEcosystem().toLowerCase(), Handler.class);
             return handler.handle(scanRequest);
-        } catch(Exception ignored) {
-            return ResponseEntity.badRequest().body(String.format("Invalid param value - '%s' ecosystem is not supported currently", scanRequest.getEcosystem()));
+        } catch(BeansException ignored) {
+            return ResponseEntity.badRequest().body(new MessageResponse(String.format("Invalid param value - '%s' ecosystem is not supported currently", scanRequest.getEcosystem())));
+        } catch(Exception ex) {
+            return ResponseEntity.badRequest().body(new MessageResponse(ex.getMessage()));
         }
     }
 }
